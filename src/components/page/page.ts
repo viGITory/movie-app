5a15ab06998e12e331f27cad2dbbce3f5dceb2f8
PANTORY <pantory@gmail.com>
2022-02-17 13:13:28 +0300

    feat: add method to create movie cards

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,3 +1,10 @@
+interface IData {
+  overview: string;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+}
+
 export default class Page {
   container: HTMLDivElement;
   main!: HTMLDivElement;
@@ -46,8 +53,31 @@ export default class Page {
     return data;
   };
 
+  private addMovies = async (): Promise<void> => {
+    try {
+      const data = await this.getData(
+        'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=48fa0c325cf33db96de5b585427f9aa1'
+      );
+
+      data.results.forEach((item: IData) => {
+        let movie = document.createElement('article');
+        movie.classList.add('movie');
+
+        movie.innerHTML = `
+          <h3 class="movie__title">${item.title}</h3>
+          <img class="movie__poster" src="https://image.tmdb.org/t/p/w1280${item.poster_path}">
+          <p class="movie__overview">${item.overview}</p>
+          <p class="movie__rate">${item.vote_average}</p>
+        `;
+
+        this.main.append(movie);
+      });
+    } catch (err) {}
+  };
+
   public init = (): void => {
     this.render();
     this.getElements();
+    this.addMovies();
   };
 }
