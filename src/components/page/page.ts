19879cdb34d81f9db82f87b45be6d87df6c595c9
PANTORY <pantory@gmail.com>
2022-02-20 11:18:59 +0300

    refactor: add separate file for TS types

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,13 +1,4 @@
-interface IData {
-  first_air_date: string;
-  release_date: string;
-  backdrop_path: string;
-  name: string;
-  overview: string;
-  poster_path: string;
-  title: string;
-  vote_average: number;
-}
+import { IData } from '../../scripts/types';
 
 export default class Page {
   container: HTMLDivElement;
---
+++ src/scripts/types.ts
@@ -0,0 +1,10 @@
+export interface IData {
+  first_air_date: string;
+  release_date: string;
+  backdrop_path: string;
+  name: string;
+  overview: string;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+}
