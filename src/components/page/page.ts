eb4348ec7135274507efee2e209060ba94e935d9
PANTORY <pantory@gmail.com>
2022-03-15 14:29:16 +0300

    refactor: replace theme key name in local storage

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -166,11 +166,15 @@ export default class Page {
 
   private addListeners = (): void => {
     window.addEventListener('beforeunload', () => {
-      localStorage.setItem('theme', document.documentElement.className);
+      localStorage.setItem(
+        'vigitory-movieapp-theme',
+        document.documentElement.className
+      );
     });
 
     document.addEventListener('DOMContentLoaded', () => {
-      document.documentElement.className = localStorage.getItem('theme') || '';
+      document.documentElement.className =
+        localStorage.getItem('vigitory-movieapp-theme') || '';
     });
 
     document.addEventListener('keyup', (event: KeyboardEvent) => {
